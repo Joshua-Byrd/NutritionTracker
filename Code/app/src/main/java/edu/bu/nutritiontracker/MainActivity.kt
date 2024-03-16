4e0713429405fe144241ab1e1f768e6b36916de4
Joshua Byrd <joshuabyrd@outlook.com>
2024-03-16 09:58:12 -0400

    Create MainDisplay, Summary, FoodList composables; Start laying out activities

--- Code/app/src/main/java/edu/bu/nutritiontracker/MainActivity.kt
+++ Code/app/src/main/java/edu/bu/nutritiontracker/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.bu.nutritiontracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,25 +23,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainDisplay("03/16/2024", Modifier)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+/**
+ * Displays date, nutrition summary and list of foods eaten
+ *  ---NEEDS TO ACCEPT A DATE TO DISPLAY (Date class?)---
+ */
 @Composable
-fun GreetingPreview() {
-    NutritionTrackerTheme {
-        Greeting("Android")
+fun MainDisplay(date: String, modifier: Modifier) {
+    Column {
+        Text(date)
     }
 }
+
+/**
+ * Displays summary of calories and macronutrients
+ */
+@Composable
+fun Summary(
+    calories: Int,
+    carbs: Int,
+    protein: Int,
+    fat: Int,
+    satFat: Int,
+    fiber: Int
+    ){}
+
+/**
+ * Displays list of foods eaten ---NEEDS TO ACCEPT A LIST OF FOOD OBJECTS---
+ */
+@Composable
+fun FoodList(){}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true)
+@Composable
+fun MainDisplayPreview() {
+    MainDisplay(date = "03/16/2024", Modifier)
+}
+
+/**
+ * TO DO FOR ITERATION 1
+ *
+ * 1. Create Food class
+ * 2. Write functions: MainDisplay, FoodList, Summary
+ * 3. MainActivity.kt should be able to display info from any date
+ * 4. Create Activity for Searching for foods
+ *
+ */
