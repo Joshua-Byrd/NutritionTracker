2e48a2b5cd2467c06b9977a93a2b995dcb956d1b
Joshua Byrd <joshuabyrd@outlook.com>
2024-04-19 09:12:17 -0400

    Add Fonts

--- Code/app/src/main/java/edu/bu/nutritiontracker/ui/theme/Type.kt
+++ Code/app/src/main/java/edu/bu/nutritiontracker/ui/theme/Type.kt
@@ -5,12 +5,20 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.ExtraBold
+import androidx.compose.ui.text.font.FontWeight.Companion.Normal
 import androidx.compose.ui.unit.sp
 import edu.bu.nutritiontracker.R
 
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_thin, FontWeight.Thin),
+    Font(R.font.montserrat_extrabold, FontWeight.Bold),
+    Font(R.font.montserrat_regular, FontWeight.Normal)
+)
+
 val AppTypography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -18,8 +26,8 @@ val AppTypography = Typography(
     ),
 
     titleLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
